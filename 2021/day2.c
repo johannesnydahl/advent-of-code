bccefd102f1e76dae07370f6c04816a07ef2009b
IceDops <hello@icedops.com>
2021-12-02 23:01:39 +0100

    Day 2

--- 2021/day2.c
+++ 2021/day2.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char lineData[6];
+char lineData[11];
 int forward = 0;
-int up = 0;
-int down = 0;
+int depth = 0;
+int currentNumber = 0;
+int aim = 0;
 
 int main() {
-    FILE * inputFile = fopen ("./input/day2.txt", "r");
+    FILE * inputFile = fopen("./input/day2.txt", "r");
 
-    while (fgets(lineData, 6, inputFile) != NULL) {
-        printf("%c\n", lineData[0]);
-        if (lineData[0] == u") {
-
-        } else if (lineData[0] == "d") {
-
-        } else if (lineData[0] == "f") {
-
+    while (fgets(lineData, 11, inputFile) != NULL) {
+        currentNumber = atoi(&lineData[strcspn(lineData, "\n") - 1]);        
+        if (lineData[0] == 'u') {
+            depth -= currentNumber;
+        } else if (lineData[0] == 'd') {
+            depth += currentNumber;
+        } else if (lineData[0] == 'f') {
+            forward += currentNumber;
         }
       
     }
-    return 0;
 
+    printf("Part 1: %i\n", depth*forward);
+
+    depth = 0;  
+    forward = 0; 
+
+    fclose(inputFile);
+    inputFile = fopen("./input/day2.txt", "r");
+
+    while (fgets(lineData, 11, inputFile) != NULL) {
+        currentNumber = atoi(&lineData[strcspn(lineData, "\n") - 1]);        
+        if (lineData[0] == 'u') {
+            aim -= currentNumber;
+        } else if (lineData[0] == 'd') {
+            aim += currentNumber;
+        } else if (lineData[0] == 'f') {
+            forward += currentNumber;
+            depth += aim * currentNumber;
+        }
+
+    }
+
+    printf("Part 2: %i\n", depth*forward);
+    return 0;
 }
