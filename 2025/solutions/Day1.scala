9b814c4dff101b60f5ac8e5872c99a3ebce276f5
Johannes Nydahl <johannesnydahl@protonmail.com>
2025-12-01 11:37:43 +0100

    added day1 part 2

--- 2025/solutions/Day1.scala
+++ 2025/solutions/Day1.scala
@@ -22,3 +22,31 @@ object Day1 extends Day(1):
         end for
 
         println(sum)
+        // Part 1 done
+
+        pos = 50
+        sum = 0
+
+        def turnDial(dir: Dir): Unit =
+            dir match
+                case Dir.Left =>
+                    val tempPos = pos - 1
+                    if tempPos < 0 then pos = 99 else pos = tempPos
+                case Dir.Right =>
+                    pos = (pos + 1) % 100
+
+        for i <- input do
+            val ticks = i.substring(1).toInt
+            val dir = i(0) match
+                case 'L' =>
+                    for j <- 0 until ticks do
+                        turnDial(Dir.Left)
+                        if pos == 0 then sum += 1
+                case 'R' =>
+                    for j <- 0 until ticks do
+                        turnDial(Dir.Right)
+                        if pos == 0 then sum += 1
+                case _ => throw Exception("Neither L nor R")
+        end for
+
+        println(sum)
