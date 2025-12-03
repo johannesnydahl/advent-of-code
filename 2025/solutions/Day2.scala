8edeecd23d0ccb36f0e14c5d2766e1da9877fd0e
Johannes Nydahl <johannesnydahl@protonmail.com>
2025-12-03 17:45:38 +0100

    add day2 part 2

--- 2025/solutions/Day2.scala
+++ 2025/solutions/Day2.scala
@@ -2,7 +2,7 @@ package solutions
 
 object Day2 extends Day(2):
     @main def main2 =
-        // Life would be easier if I used longs.
+        // Life would be easier if I used longs...
         val ranges: Seq[(BigInt, BigInt)] = input(0).split(",").map(parseRange)
         var sum = BigInt(0);
 
@@ -14,6 +14,20 @@ object Day2 extends Day(2):
                 val split = s.splitAt(s.length() / 2)
 
                 if split(0) == split(1) then sum = sum + i
+                i = i + 1
+
+        println(sum)
+
+        // Part 2
+
+        sum = BigInt(0);
+
+        for range <- ranges yield
+            var i = range._1
+            while i <= range._2 do
+                val s = i.toString()
+
+                if """^(\w+)\1+$""".r matches s then sum = sum + i
                 i = i + 1
 
         println(sum)
